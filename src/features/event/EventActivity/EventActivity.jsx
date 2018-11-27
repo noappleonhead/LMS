e7570844f563221e4b4f30914aae717953bf672f
RH <RENE.H.OK@GMAIL.COM>
2018-11-27 22:01:10 +1100

    cloud function done

--- src/features/event/EventActivity/EventActivity.jsx
+++ src/features/event/EventActivity/EventActivity.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
-import { Header, Segment } from 'semantic-ui-react'
+import { Header, Segment, Feed, Sticky } from 'semantic-ui-react'
+import EventActivityItem from './EventActivityItem';
 
-const EventActivity = () => {
+const EventActivity = ({activities, contextRef}) => {
   return (
+    <Sticky context={contextRef} offset={100}>
     <div>
       <Header attached='top' content='Recent Activity'/>
       <Segment attached>
-        <p>Recent activity</p>
+        <Feed>
+          {activities && activities.map((activity) => (
+            <EventActivityItem key={activities.id} activity={activity}/>
+          ))}
+        </Feed>
       </Segment>
     </div>
+    </Sticky>
   )
 }
 
